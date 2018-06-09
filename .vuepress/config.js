985de65682b31975f7c1dfadfeeec9113676b7a8
Maurice Williams <maurice.williams@nurocor.com>
2018-06-09 15:35:48 -0400

    first commit

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'Maurice Williams - Portfolio',
+    description: 'A place to showcase the things I\'ve done',
+    base: '/portfolio/',
+    head: [
+        ['meta', {'http-equiv': 'X-UA-Compatible', content: 'IE=edge'}]
+    ],
+    dest: 'docs',
+    serviceWorker: true,
+    ga: 'UA-74377776-1'
+}
