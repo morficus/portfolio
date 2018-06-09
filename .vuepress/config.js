e50369cf857e848e66f3156c83bc9bf4433eca93
Maurice Williams <morficus@gmail.com>
2018-06-09 17:15:39 -0400

    updating config to include navigation

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,28 @@
 module.exports = {
     title: 'Maurice Williams - Portfolio',
     description: 'A place to showcase the things I\'ve done',
-    base: '/portfolio/',
+    themeConfig: {
+        nav: [
+            { text: 'My Work', link: '/portfolio' },
+            { text: 'Career', link: '/resume' },
+            { text: 'Hire me', link: 'https://www.upwork.com/fl/mauricew24'},
+            { text: 'Get in touch', items: [
+                    { text: 'Twitter', link: 'https://www.twitter.com/morficus' },
+                    { text: 'GitHub', link: 'https://www.github.com/morficus' },
+                    { text: 'LinkedIn', link: 'https://www.linkedin.com/in/mauricew/' },
+                    { text: 'Email', link: '/contact' }
+                ] },
+        ],
+        sidebar: [
+            '/',
+            ['portfolio/', 'My Work'],
+            ['resume/', 'My Career'],
+            // ['https://www.upwork.com/fl/mauricew24', 'Hire me']
+        ],
+        lastUpdated: true,
+    },
+
+    // base: '/portfolio/',
     head: [
         ['meta', {'http-equiv': 'X-UA-Compatible', content: 'IE=edge'}]
     ],
