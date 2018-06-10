e575ae418361dd677c2133236d47b291d5b87a86
Maurice Williams <morficus@gmail.com>
2018-06-09 23:57:38 -0400

    generating new site to see what it looks like on GitHub

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -21,27 +21,107 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "8412f7e14e12f7c9ed9cf0e4762c7217"
+    "revision": "a407f762dc941ae6d21d60efd7ec0185"
   },
   {
-    "url": "assets/css/1.styles.e3518659.css",
+    "url": "assets/css/9.styles.aa18329c.css",
     "revision": "c875a569e3a65d9584f3fb5119d41920"
+  },
+  {
+    "url": "assets/img/bandwidth-utilization-comparison.44fd17a7.png",
+    "revision": "44fd17a7bb824fb2cd6537e393a3eea0"
+  },
+  {
+    "url": "assets/img/grow-calculator-dashboard.b588bb5e.gif",
+    "revision": "b588bb5ea044a772a658d306bbc59197"
+  },
+  {
+    "url": "assets/img/grow-calculator-landing.e5add5b5.png",
+    "revision": "e5add5b5c7daa6c9388ec4ebde960117"
+  },
+  {
+    "url": "assets/img/grow-calculator-location.0637bf80.gif",
+    "revision": "0637bf800fe379eb7fe5c2859b0b5103"
   },
   {
     "url": "assets/img/search.83621669.svg",
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/0.45c6d715.js",
-    "revision": "fe241e4dcd830a97d76b2d05faaa4859"
+    "url": "assets/js/0.9b4c1c16.js",
+    "revision": "e95626bde9fb1ab2ef045d8e070ac3af"
   },
   {
-    "url": "assets/js/app.a16d263f.js",
-    "revision": "2d41336b2fd3d25960b701352e30c2fa"
+    "url": "assets/js/1.b05615f8.js",
+    "revision": "3f4b7dc9914ec85a12541ff7d1dda668"
+  },
+  {
+    "url": "assets/js/2.c85ba912.js",
+    "revision": "e582e88492fcdfc03c466d3a8d72a6a7"
+  },
+  {
+    "url": "assets/js/3.7a69b41c.js",
+    "revision": "2cc400945998e42ad16ae31bc79a5117"
+  },
+  {
+    "url": "assets/js/4.e505ed15.js",
+    "revision": "3619596cdc02e3abf0d56b0b67a68c5e"
+  },
+  {
+    "url": "assets/js/5.1cd24b77.js",
+    "revision": "36299c1e423c0d6e06874330c276e989"
+  },
+  {
+    "url": "assets/js/6.59fcac3c.js",
+    "revision": "2c8d4ae1ebd903939ec919084eb9ffda"
+  },
+  {
+    "url": "assets/js/7.96744318.js",
+    "revision": "c3836d2f2c5ac41fd0704dcdd4522117"
+  },
+  {
+    "url": "assets/js/8.f6bfe5c0.js",
+    "revision": "2c33772d5378f144ba8602a63ed63571"
+  },
+  {
+    "url": "assets/js/app.3fa50846.js",
+    "revision": "f3aca72017dcb2ff42076e758159c419"
+  },
+  {
+    "url": "HOME.html",
+    "revision": "7ca5d0f4468985745187c26b99549e5c"
   },
   {
     "url": "index.html",
-    "revision": "d1373d106b408569f132ae97d2e7079f"
+    "revision": "f624bdb54d2f5c2b03054f2b24743235"
+  },
+  {
+    "url": "portfolio/grow-vs-buy-calculator.html",
+    "revision": "f65169052c2e32af6af4cb82cb5289ca"
+  },
+  {
+    "url": "portfolio/index.html",
+    "revision": "956362274e70734e7dcd894bfa9090d6"
+  },
+  {
+    "url": "portfolio/music-analyzer.html",
+    "revision": "d205f74fbddb206feeed300d13bd739c"
+  },
+  {
+    "url": "portfolio/simple-as-can-be.html",
+    "revision": "a612084bc4bfe6199b4b72a9a6540e17"
+  },
+  {
+    "url": "portfolio/telemetry-data-viz.html",
+    "revision": "6b9e745789210684b27adc4b2ec5484d"
+  },
+  {
+    "url": "portfolio/xml-to-rest-api-facaed.html",
+    "revision": "1ebbe0213f9cbdadecdc67c8b4b3a558"
+  },
+  {
+    "url": "resume/index.html",
+    "revision": "68b7862ca2f815ef4af14c04bf7b1523"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
