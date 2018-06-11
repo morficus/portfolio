c9e585b3bf46eae6b47a5a60d60a3765e8d5aaba
Maurice Williams <morficus@gmail.com>
2018-06-11 00:45:24 -0400

    Generating new version

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -21,7 +21,7 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "a407f762dc941ae6d21d60efd7ec0185"
+    "revision": "136a0812a78418d185abf08791e80aee"
   },
   {
     "url": "assets/css/9.styles.aa18329c.css",
@@ -44,84 +44,100 @@ self.__precacheManifest = [
     "revision": "0637bf800fe379eb7fe5c2859b0b5103"
   },
   {
+    "url": "assets/img/musical-turk-file-upload.3a893501.gif",
+    "revision": "3a893501cee1f845cacd1222a2f92b06"
+  },
+  {
+    "url": "assets/img/musical-turk-landing.65e92bac.png",
+    "revision": "65e92bacf4cdda3df9fb02c92e5b27ae"
+  },
+  {
+    "url": "assets/img/musical-turk-test-coverage-1.c773a7e7.png",
+    "revision": "c773a7e7147a460e64d74e0315ef4b06"
+  },
+  {
+    "url": "assets/img/musical-turk-test-coverage-2.34bea86e.png",
+    "revision": "34bea86ea19d89b2b958ce569d845fa5"
+  },
+  {
     "url": "assets/img/search.83621669.svg",
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/0.9b4c1c16.js",
-    "revision": "e95626bde9fb1ab2ef045d8e070ac3af"
+    "url": "assets/js/0.5c0a2243.js",
+    "revision": "cb1fd705ecd87d23f566978ad50c01a4"
   },
   {
-    "url": "assets/js/1.b05615f8.js",
-    "revision": "3f4b7dc9914ec85a12541ff7d1dda668"
+    "url": "assets/js/1.8533c7d0.js",
+    "revision": "edc376b990434f1df511e5d8ff4468a1"
   },
   {
-    "url": "assets/js/2.c85ba912.js",
-    "revision": "e582e88492fcdfc03c466d3a8d72a6a7"
+    "url": "assets/js/2.b82b7f44.js",
+    "revision": "a22cfc19cbf1294f1f313b3aedd64e6b"
   },
   {
-    "url": "assets/js/3.7a69b41c.js",
-    "revision": "2cc400945998e42ad16ae31bc79a5117"
+    "url": "assets/js/3.c12699ea.js",
+    "revision": "dd8bc9506aaeb1ee1f6e9f91e5081eb6"
   },
   {
-    "url": "assets/js/4.e505ed15.js",
-    "revision": "3619596cdc02e3abf0d56b0b67a68c5e"
+    "url": "assets/js/4.e25da5f7.js",
+    "revision": "e42022abccfbb8d5c79a30cecb019700"
   },
   {
-    "url": "assets/js/5.1cd24b77.js",
-    "revision": "36299c1e423c0d6e06874330c276e989"
+    "url": "assets/js/5.0677b5c9.js",
+    "revision": "7f88c10cfe8c2406e602e1be78406508"
   },
   {
-    "url": "assets/js/6.59fcac3c.js",
-    "revision": "2c8d4ae1ebd903939ec919084eb9ffda"
+    "url": "assets/js/6.c5e4d489.js",
+    "revision": "c5dd6effa05a1f7750be899653397ab0"
   },
   {
-    "url": "assets/js/7.96744318.js",
-    "revision": "c3836d2f2c5ac41fd0704dcdd4522117"
+    "url": "assets/js/7.227746ed.js",
+    "revision": "72f093e5993481ad69f5b765385b8eb1"
   },
   {
-    "url": "assets/js/8.f6bfe5c0.js",
-    "revision": "2c33772d5378f144ba8602a63ed63571"
+    "url": "assets/js/8.f08eae5c.js",
+    "revision": "8be326fd6a820fe31faf3644bd8aa048"
   },
   {
-    "url": "assets/js/app.3fa50846.js",
-    "revision": "f3aca72017dcb2ff42076e758159c419"
+    "url": "assets/js/app.357a2071.js",
+    "revision": "cc6db48b03588077c6c959c5252aae4e"
   },
   {
     "url": "HOME.html",
-    "revision": "7ca5d0f4468985745187c26b99549e5c"
+    "revision": "314b3fe5cea521bf0ef2f7e1422bc3a5"
   },
   {
     "url": "index.html",
-    "revision": "f624bdb54d2f5c2b03054f2b24743235"
+    "revision": "10fd84f3186f50beb5109ef56ecb1011"
   },
   {
     "url": "portfolio/grow-vs-buy-calculator.html",
-    "revision": "f65169052c2e32af6af4cb82cb5289ca"
+    "revision": "8216fcca951596dcb7ed4ff6977ac436"
   },
   {
     "url": "portfolio/index.html",
-    "revision": "956362274e70734e7dcd894bfa9090d6"
+    "revision": "edcb9dbdc9c6aba0b1b51b56f6d131f7"
   },
   {
-    "url": "portfolio/music-analyzer.html",
-    "revision": "d205f74fbddb206feeed300d13bd739c"
+    "url": "portfolio/musical-turk.html",
+    "revision": "e09b770b4f18a2b5e1948ea617c21069"
   },
   {
     "url": "portfolio/simple-as-can-be.html",
-    "revision": "a612084bc4bfe6199b4b72a9a6540e17"
+    "revision": "780771b345fa9257e4ec8b2f67aeead9"
   },
   {
     "url": "portfolio/telemetry-data-viz.html",
-    "revision": "6b9e745789210684b27adc4b2ec5484d"
+    "revision": "905caf58e5e27b466cd67f22bf5bc625"
   },
   {
     "url": "portfolio/xml-to-rest-api-facaed.html",
-    "revision": "1ebbe0213f9cbdadecdc67c8b4b3a558"
+    "revision": "fb865059a8b0e66f5d3606a81cabf5b8"
   },
   {
     "url": "resume/index.html",
-    "revision": "68b7862ca2f815ef4af14c04bf7b1523"
+    "revision": "5f5e18af0d5d0e7269c397a97ca25a86"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
