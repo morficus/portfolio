b3894af607fc71ff17280797e5bab4ca01e0fd25
Maurice Williams <morficus@gmail.com>
2018-08-23 09:37:04 -0700

    generating new static site for new base URL

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -11,7 +11,7 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -21,11 +21,11 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "136a0812a78418d185abf08791e80aee"
+    "revision": "9cd36ba59d4332ffaf2eacd83144d257"
   },
   {
-    "url": "assets/css/9.styles.aa18329c.css",
-    "revision": "c875a569e3a65d9584f3fb5119d41920"
+    "url": "assets/css/0.styles.4568b041.css",
+    "revision": "2ed4c8df4ff33bc36857f179247b429c"
   },
   {
     "url": "assets/img/bandwidth-utilization-comparison.44fd17a7.png",
@@ -64,81 +64,101 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/0.5c0a2243.js",
-    "revision": "cb1fd705ecd87d23f566978ad50c01a4"
+    "url": "assets/js/10.abc5bdda.js",
+    "revision": "6212dd1b9e462758dbb41982d36e1631"
   },
   {
-    "url": "assets/js/1.8533c7d0.js",
-    "revision": "edc376b990434f1df511e5d8ff4468a1"
+    "url": "assets/js/11.9d4d9509.js",
+    "revision": "67271d714c4219e964ee7113477d65ad"
   },
   {
-    "url": "assets/js/2.b82b7f44.js",
-    "revision": "a22cfc19cbf1294f1f313b3aedd64e6b"
+    "url": "assets/js/12.10465731.js",
+    "revision": "d4df6bfc4a6f0eb9e8e9215e02898556"
   },
   {
-    "url": "assets/js/3.c12699ea.js",
-    "revision": "dd8bc9506aaeb1ee1f6e9f91e5081eb6"
+    "url": "assets/js/2.861402b7.js",
+    "revision": "2ff2705e890b878913a9dfcd10aada51"
   },
   {
-    "url": "assets/js/4.e25da5f7.js",
-    "revision": "e42022abccfbb8d5c79a30cecb019700"
+    "url": "assets/js/3.c95546f0.js",
+    "revision": "015098ad26b05f91df9cf99db480d128"
   },
   {
-    "url": "assets/js/5.0677b5c9.js",
-    "revision": "7f88c10cfe8c2406e602e1be78406508"
+    "url": "assets/js/4.ddcbf3d9.js",
+    "revision": "9300e994bc90d6441fab5b107433ad27"
   },
   {
-    "url": "assets/js/6.c5e4d489.js",
-    "revision": "c5dd6effa05a1f7750be899653397ab0"
+    "url": "assets/js/5.80a36e56.js",
+    "revision": "61c5ae44d6f1b5cc0e8fe4181da74f11"
   },
   {
-    "url": "assets/js/7.227746ed.js",
-    "revision": "72f093e5993481ad69f5b765385b8eb1"
+    "url": "assets/js/6.5faed990.js",
+    "revision": "c4f3af99a2504b4d0ef909ff7a0e3e34"
   },
   {
-    "url": "assets/js/8.f08eae5c.js",
-    "revision": "8be326fd6a820fe31faf3644bd8aa048"
+    "url": "assets/js/7.52339273.js",
+    "revision": "96996863a3e0e612acaa720bb5ae3c8b"
   },
   {
-    "url": "assets/js/app.357a2071.js",
-    "revision": "cc6db48b03588077c6c959c5252aae4e"
+    "url": "assets/js/8.ebc906bb.js",
+    "revision": "20f13034e6ed42e3e1ca4e4e846fd3bb"
+  },
+  {
+    "url": "assets/js/9.2de94c6f.js",
+    "revision": "6f524d603a883624f13fc2b983ef6d7c"
+  },
+  {
+    "url": "assets/js/app.57bf59ce.js",
+    "revision": "377f0e04d3577e765d43ef4116b74a66"
   },
   {
     "url": "HOME.html",
-    "revision": "314b3fe5cea521bf0ef2f7e1422bc3a5"
+    "revision": "15dea430a814fa1195d96af77455d7c5"
   },
   {
     "url": "index.html",
-    "revision": "10fd84f3186f50beb5109ef56ecb1011"
+    "revision": "b4de169146268592b5078f519a719240"
   },
   {
     "url": "portfolio/grow-vs-buy-calculator.html",
-    "revision": "8216fcca951596dcb7ed4ff6977ac436"
+    "revision": "e894ddc53e3b2390181290a629fcc47d"
   },
   {
     "url": "portfolio/index.html",
-    "revision": "edcb9dbdc9c6aba0b1b51b56f6d131f7"
+    "revision": "26d030a9c5bf16216fb31933495d1970"
   },
   {
     "url": "portfolio/musical-turk.html",
-    "revision": "e09b770b4f18a2b5e1948ea617c21069"
+    "revision": "5648f66e258b26d65bf3d1838abd210f"
   },
   {
     "url": "portfolio/simple-as-can-be.html",
-    "revision": "780771b345fa9257e4ec8b2f67aeead9"
+    "revision": "7b996324c663d2d8c4f7e7459ea5b2c6"
   },
   {
     "url": "portfolio/telemetry-data-viz.html",
-    "revision": "905caf58e5e27b466cd67f22bf5bc625"
+    "revision": "52cf3bcaba0667ef89392bffe0e46a04"
   },
   {
     "url": "portfolio/xml-to-rest-api-facaed.html",
-    "revision": "fb865059a8b0e66f5d3606a81cabf5b8"
+    "revision": "de80ca2821b4118ecda905f9575a1005"
   },
   {
     "url": "resume/index.html",
-    "revision": "5f5e18af0d5d0e7269c397a97ca25a86"
+    "revision": "247b937538a906f212cfe84e81ec9cfb"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+addEventListener('message', event => {
+  const replyPort = event.ports[0]
+  const message = event.data
+  if (replyPort && message && message.type === 'skip-waiting') {
+    event.waitUntil(
+      self.skipWaiting().then(
+        () => replyPort.postMessage({ error: null }),
+        error => replyPort.postMessage({ error })
+      )
+    )
+  }
+})
